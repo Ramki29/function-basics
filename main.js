9da3db0222e187fbd72dc388f4a285fb7648319b
Ramki29 <ramkie7010@gmail.com>
2023-03-24 21:52:07 +0530

    add function basics

---
+++ main.js
@@ -0,0 +1,47 @@
+
+name();
+document.write("<br>");
+var i=0;
+for(i=0; i<=4; i++)
+	{
+	document.write(i);
+	document.write("<br>");
+	}
+	
+	
+	name();
+	document.write("<br>");
+function name()
+	{
+	document.write("hello");
+	}
+	name();
+	document.write("<br>");
+	
+	
+function nam(a)
+	{
+	document.write(a);
+	}
+	nam("hello");
+	document.write("<br>");
+	nam("world");
+	document.write("<br>");
+	nam("song");
+	document.write("<br>");
+	
+	
+function nama(a,b)
+	{
+	document.write(a,b);
+	document.write("<br>");
+	}
+	nama(10,10);
+	
+var a=prompt("enter the name");
+function namee(a)
+	{		
+	document.write(a);
+	}
+	namee(a);
+	
